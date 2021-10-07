d26d496014b9fd285af2d11cba014ca2732fba3f
Chandra <88462784+ChandraLis@users.noreply.github.com>
2021-10-07 17:44:06 -0400

    update

--- script.js
+++ script.js
@@ -4,14 +4,7 @@
 //const url = 'api.giphy.com/v1/gifs/search' // this is the basic endpoint for searching, instead of random, may need more specific info
 const url = 'https://api.giphy.com/v1/gifs/random';
 const key = 'bSOtb0Zx6WXd32I0zEIUc0uv41tKAYmG';
-
-const button = document.querySelector("cat me");
-//button.addEventListener("click",(getImage));
-
-/*
-let image = document.getElementById('image')
-let img = document.createElement('img');
-    img.src = 
+let url;
 /*
 $.get({ 
 url: "https://api.giphy.com/v1/gifs/search?= cat+form.data&api_key=bSOtb0Zx6WXd32I0zEIUc0uv41tKAYmG=pg ",// can we tie form.data to the results?
@@ -25,34 +18,15 @@ error: function(error) {
 });
 */
 
-//*FETCH
 
-fetch(url+'?api_key='+ key + '&limit=20')
+fetch(url)
 .then(function(result) {
     console.log(result)
     return result.json();
 }).then(function(json) {
     console.log(json);
-}).catch(function(err){
-    console.log(err);
-})
+});
 
-
-function fetchResults(e) {
-    fetch(url).then(function(result) {
-        return result.json();
-    }).then(function(json) {
-        displayResults(json);  
-    });
-}
-
-/*function displayResults(json) {
-    console.log(json.url);
-    let imgUrl = json.url;
-    image.src = imgUrl;
-};
-
-*/
 
 //do I need a for loop to search out cat in the standard 25 results returned for the one word(mood)query put through the form?
 
